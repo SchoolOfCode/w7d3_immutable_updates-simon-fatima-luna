848ded8d9fe3e564f8cf195e5abf1d6984270df1
Simon Prato <simonprato11@gmail.com>
2022-01-06 13:30:32 -0300

    last task

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ export function replaceItem(array, item, index) {
 
 // Immutably remove an item at a specific position/index within an array
 export function removeItem(array, index) {
-    const newArray = [...array.slice(0, index),...array.slice(index + 1)];
-    // array = [1, 2, 3, 4, 5, 6]
-    // slice(0, 2) === [1, 2]
-    // slice(3) === [4, 5, 6]
-    return newArray;
+  const newArray = [...array.slice(0, index), ...array.slice(index + 1)]
+  // array = [1, 2, 3, 4, 5, 6]
+  // slice(0, 2) === [1, 2]
+  // slice(3) === [4, 5, 6]
+  return newArray
 }
 
 //Objects:
@@ -45,8 +45,8 @@ export function removeItem(array, index) {
 // should give back:
 //     { name: "Barbara"}
 export function updateName(object, newName) {
-    const newArray = {...object, name: newName};
-    return newArray
+  const newArray = { ...object, name: newName }
+  return newArray
 }
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
@@ -56,16 +56,24 @@ export function updateName(object, newName) {
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
 export function toggleTeaStatus(object) {
-
-  return {...object, needsACupOfTea: !object.needsACupOfTea}
+  return { ...object, needsACupOfTea: !object.needsACupOfTea }
 }
 
 // Combo Time!!
 
-// Immutably update the array as well as the object at the given position/index within the array, so that the value under the "completed" property becomes the opposite of what it was.
+// Immutably update the array as well as the object at the given position/index within the array,
+// so that the value under the "completed" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
 // For example calling:
 //    toggleListItemCompleted([{ task: "Cooking", completed: true }, { task: "Walking", completed: false }], 1)
 // should give back:
 //    [{ task: "Cooking", completed: true }, { task: "Walking", completed: true }]
-export function toggleListItemCompleted(array, index) {}
+export function toggleListItemCompleted(array, index) {
+  return [
+    ...array.slice(0, index),
+
+    { ...array[index], completed: !array[index].completed },
+
+    ...array.slice(index + 1),
+  ]
+}
